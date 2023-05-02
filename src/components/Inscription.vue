69fe0aa07fbfc08cfefb0658f08de7073efe564c
Mecene Phrygien <mecenephrygien@gmail.com>
2023-05-02 08:55:18 +0300

    filtre etudiant

--- src/components/Inscription.vue
+++ src/components/Inscription.vue
@@ -1,5 +1,50 @@
 <template>
-    <h1>Liste Inscription</h1>
+    <h1>Liste étudiants</h1>
+    <br>
+    <a href="" class="btn btn-success">Enregistrer étudiant</a>
+
+    <div class="row mt-2">
+        <div class="col-md-3">
+            <select class="form-control">
+                <option>Filtrer par mention</option>
+                <option>INFORMATIQUE</option>
+                <option>MANAGEMENT</option>
+                <option>MULTIMEDIA</option>
+            </select>
+        </div>
+
+        
+        <div class="col-md-3">
+            <select class="form-control">
+                <option>Filtrer par cycle</option>
+                <option>LICENCE</option>
+                <option>MASTER</option>
+            </select>
+        </div>
+
+        <div class="col-md-3">
+            <select class="form-control">
+                <option>Filtrer par parcours</option>
+                <option>DA2I</option>
+                <option>SIGD</option>
+                <option>M2I</option>
+                <option>MD</option>
+                <option>RPM</option>
+            </select>
+        </div>
+
+        <div class="col-md-3">
+            <select class="form-control">
+                <option>Filtrer par niveau</option>
+                <option>L1</option>
+                <option>L2</option>
+                <option>L3</option>
+                <option>M1</option>
+                <option>M2</option>
+            </select>
+        </div>
+
+    </div>
     <hr>
     <div class="col-md-12">
         <div class="row">
